eeb5d900427aac34ed77c35332e9360045d0901a
한승우 <tmddn_00@naver.com>
2026-05-04 14:06:36 +0900

    [refact] post entity 폴더 위치 수정

--- src/main/java/io/backend/blogproject/domain/entity/Post.java
+++ src/main/java/io/backend/blogproject/domain/entity/Post.java
@@ -1,4 +1,4 @@
-package io.backend.blogproject.dto.domain;
+package io.backend.blogproject.domain.entity;
 
 import io.backend.blogproject.constant.Status;
 import io.backend.blogproject.constant.Visibility;
