2dc505267bf55971982b1372394be0ec34fcfce6
KF-16 <wjdtn747@naver.com>
2026-05-04 17:43:38 +0900

    [REFACTOR] 오류 코드 수정

--- src/main/java/io/backend/blogproject/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package io.backend.blogproject.repository;
-
-import io.backend.blogproject.domain.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
