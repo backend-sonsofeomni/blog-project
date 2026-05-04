37774adfd319c27501ab122ed14a1f260a738df7
KF-16 <wjdtn747@naver.com>
2026-05-04 16:04:29 +0900

    [FEAT] Comment Repository - 게시글 조회 기능 추가

---
+++ src/main/java/io/backend/blogproject/repository/CommentRepository.java
@@ -0,0 +1,35 @@
+package io.backend.blogproject.repository;
+
+import io.backend.blogproject.constant.ErrorCode;
+import io.backend.blogproject.domain.entity.Comment;
+import io.backend.blogproject.domain.entity.Post;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepository {
+    private final EntityManagerFactory emf;
+
+    public List<Comment> findCommentsByPostId(Long postId){
+        try(
+                EntityManager em = emf.createEntityManager()
+        ) {
+            String JPQL_GET_COMMENTS = """
+                    SELECT c
+                    FROM Comment c
+                    WHERE c.post = :postId
+                    """;
+            return em.createQuery(JPQL_GET_COMMENTS, Comment.class)
+                    .setParameter("postId", postId)
+                    .getResultList();
+        } catch(Exception e) {
+            throw new RuntimeException(ErrorCode.UNABLE_TO_FIND_COMMENT.message,e);
+        }
+    }
+}
